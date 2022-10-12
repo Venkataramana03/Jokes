c810e2c3a1618b397b2fd0c5b3950fc6308bf570
Venkataramana03 <113780684+Venkataramana03@users.noreply.github.com>
2022-10-12 16:04:28 +0530

    First Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import HomePage from './components/HomePage';
+import PostAJoke from './components/PostAJoke';
 
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element = {<HomePage/>} />
+          <Route path="/getJokes" element = {<PostAJoke/>} />
+          
+        </Routes>        
+      </BrowserRouter>
     </div>
   );
 }
